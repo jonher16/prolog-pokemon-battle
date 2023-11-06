bb1de0f4872648fd60456e457b9d66e3a0f7deeb
jonher16 <1999.jon@gmail.com>
2023-11-06 18:10:49 +0900

    IE retrieving same feedback fix

--- IE.pl
+++ IE.pl
@@ -1,76 +1,89 @@
-% Rule for super effective attack
-feedback('The attack was super effective!', 3) :-
-    NPType = 'water',
-    EType = 'fire',
-    Intensity = 'strong'.
-
-% Rule for very effective attack
-feedback('The attack was very effective.', 2) :-
-    NPType = 'water',
-    EType = 'fire',
-    Intensity = 'normal'.
-
-% Rule for effective but weak attack
-feedback('The attack was effective but weak.', 1) :-
-    NPType = 'water',
-    EType = 'fire',
-    Intensity = 'weak'.
-
-% Rule for not effective attack
-feedback('The attack was not effective...', 0.5) :-
-    NPType = 'fire',
-    EType = 'water'.
-
-% Rule for attack dodge
-feedback('The attack was dodged!', none) :-
-    (VerbType = 'dodge' ; NPType = 'dodge').
-
-% Rule for unsuccessful dodge
-feedback('The dodge was not successful!', none) :-
-    (VerbType = 'nododge' ; NPType = 'nododge').
-
-% Rule for successful defense
-feedback('The Pokémon successfully defended.', none) :-
-    (VerbType = 'defend' ; NPType = 'defend').
-
-% Rule for broken defense
-feedback('The Pokémon defense was broken!', none) :-
-    (VerbType = 'nodefend' ; NPType = 'nodefend').
-
-% Rule for moving forward without defense or dodge
-feedback('Pokemon moved forward.', none) :-
-    Direction = 'forward',
-    \+ (VerbType = 'defend' ; NPType = 'defend' ; VerbType = 'dodge' ; NPType = 'dodge').
-
-% Rule for moving backward without defense or dodge
-feedback('Pokemon moved backward.', none) :-
-    Direction = 'backward',
-    \+ (VerbType = 'defend' ; NPType = 'defend' ; VerbType = 'dodge' ; NPType = 'dodge').
-
-% Default rule
-feedback('Unknown action', none).
-
-
 % Define a list of past tense verbs for some common verbs
 past_tense(throw, threw).
 past_tense(use, used).
-% Add more past tense forms for other verbs as needed.
+% ... (include other past tense forms as needed)
 
-% Define the verbinpast/5 predicate
-verbinpast(P1, P2, VerbWord, NPWords, Feedback1) :-
-    % Convert the verb to past tense
+% Define the verbinpast/3 predicate to construct Feedback1
+verbinpast(P1, VerbWord, NPWords, Feedback1) :-
     past_tense(VerbWord, PastVerb),
-    
-    % Combine the elements to form the feedback
     atomic_list_concat([P1, PastVerb | NPWords], ' ', Feedback1).
 
+% Define the feedback/5 predicate to construct Feedback2 and Multiplier
 
+feedback(VerbType, NPType, EType, Intensity, Direction, Feedback, Multiplier) :-
+
+     % MOVEMENT
+    (Direction == 'forward', Feedback = 'The Pokémon moved closer to the enemy!', Multiplier = 'none', !);
+    (Direction == 'backward', Feedback = 'The Pokémon moved away from the enemy!', Multiplier = 'none', !);
+
+    % DODGE
+    (VerbType == 'dodge', Feedback = 'The attack was dodged!', Multiplier = 'none', !);
+    (VerbType == 'nododge', Feedback = 'The Pokémon failed at dodging the attack!', Multiplier = 'none', !);
+
+    %DEFEND
+    (VerbType == 'defend', Feedback = 'The pokemon successfully defended itself from the enemy attack!', Multiplier = 'none', !);
+    (VerbType == 'nodefend', Feedback = 'The Pokémon failed at defending! The defense was broken!', Multiplier = 'none', !);
+
+    % WATER -> FIRE
+    (NPType == 'water', EType == 'fire', Intensity == 'strong', Feedback = 'The attack was super effective!', Multiplier = 3);
+    (NPType == 'water', EType == 'fire', Intensity == 'none', Feedback = 'The attack was very effective.', Multiplier = 2);
+    (NPType == 'water', EType == 'fire', Intensity == 'weak', Feedback = 'The attack was effective but weak.', Multiplier = 1);
+
+    % FIRE -> WATER
+    (NPType == 'fire', EType == 'water', Intensity == 'strong', Feedback = 'The attack was not very effective but dealt some damage!', Multiplier = 1);
+    (NPType == 'fire', EType == 'water', Intensity == 'none', Feedback = 'The attack was not very effective...', Multiplier = 0.5);
+    (NPType == 'fire', EType == 'water', Intensity == 'weak', Feedback = 'The attack was not effective at all...', Multiplier = 0.25);
+
+    % WATER -> GRASS
+    (NPType == 'water', EType == 'grass', Intensity == 'strong', Feedback = 'The attack was not very effective but dealt some damage!', Multiplier = 1);
+    (NPType == 'water', EType == 'grass', Intensity == 'none', Feedback = 'The attack was not very effective...', Multiplier = 0.5);
+    (NPType == 'water', EType == 'grass', Intensity == 'weak', Feedback = 'The attack was not effective at all...', Multiplier = 0.25);
+
+    % GRASS -> WATER
+    (NPType == 'grass', EType == 'water', Intensity == 'strong', Feedback = 'The attack was super effective!', Multiplier = 3);
+    (NPType == 'grass', EType == 'water', Intensity == 'none', Feedback = 'The attack was very effective.', Multiplier = 2);
+    (NPType == 'grass', EType == 'water', Intensity == 'weak', Feedback = 'The attack was effective but weak.', Multiplier = 1);
+
+    % FIRE -> GRASS
+    (NPType == 'fire', EType == 'grass', Intensity == 'strong', Feedback = 'The attack was super effective!', Multiplier = 3);
+    (NPType == 'fire', EType == 'grass', Intensity == 'none', Feedback = 'The attack was very effective.', Multiplier = 2);
+    (NPType == 'fire', EType == 'grass', Intensity == 'weak', Feedback = 'The attack was effective but weak.', Multiplier = 1);
+
+    % GRASS -> FIRE
+    (NPType == 'grass', EType == 'fire', Intensity == 'strong', Feedback = 'The attack was not very effective but dealt some damage!', Multiplier = 1);
+    (NPType == 'grass', EType == 'fire', Intensity == 'none', Feedback = 'The attack was not very effective...', Multiplier = 0.5);
+    (NPType == 'grass', EType == 'fire', Intensity == 'weak', Feedback = 'The attack was not effective at all...', Multiplier = 0.25);
+
+    % WATER -> WATER
+    (NPType == 'water', EType == 'water', Intensity == 'strong', Feedback = 'The attack was not very effective but dealt some damage!', Multiplier = 1);
+    (NPType == 'water', EType == 'water', Intensity == 'none', Feedback = 'The attack was not very effective...', Multiplier = 0.5);
+    (NPType == 'water', EType == 'water', Intensity == 'weak', Feedback = 'The attack was not effective at all...', Multiplier = 0.25);
+
+    % FIRE -> FIRE
+    (NPType == 'fire', EType == 'fire', Intensity == 'strong', Feedback = 'The attack was not very effective but dealt some damage!', Multiplier = 1);
+    (NPType == 'fire', EType == 'fire', Intensity == 'none', Feedback = 'The attack was not very effective...', Multiplier = 0.5);
+    (NPType == 'fire', EType == 'fire', Intensity == 'weak', Feedback = 'The attack was not effective at all...', Multiplier = 0.25);
+
+    % GRASS -> GRASS
+    (NPType == 'grass', EType == 'grass', Intensity == 'strong', Feedback = 'The attack was not very effective but dealt some damage!', Multiplier = 1);
+    (NPType == 'grass', EType == 'grass', Intensity == 'none', Feedback = 'The attack was not very effective...', Multiplier = 0.5);
+    (NPType == 'grass', EType == 'grass', Intensity == 'weak', Feedback = 'The attack was not effective at all...', Multiplier = 0.25);
+
+    % ALL-CASE
+    (Feedback = 'Unknown action', Multiplier = 'none').
 
 % Query to get the feedback and multiplier based on input parameters
 parse_input(VerbType, NPType, Intensity, Direction, VerbWord, NPWords, EType, P1, P2, Feedback1, Feedback2, Multiplier) :-
-    verbinpast(P1,P2,VerbWord, NPWords, Feedback1), 
-    feedback(Feedback2, Multiplier),
-    write(Feedback1), write(Feedback2), write(' (Multiplier: '), write(Multiplier), write(')'), nl.
+    write('NPType: '), write(NPType), nl,
+    write('VerbType: '), write(VerbType), nl,
+    write('VerbWord: '), write(VerbWord), nl,
+    write('EType: '), write(EType), nl,
+    write('Intensity: '), write(Intensity), nl,
+    write('Direction: '), write(Direction), nl,
+    verbinpast(P1, VerbWord, NPWords, Feedback1),
+    feedback(VerbType, NPType, EType, Intensity, Direction, Feedback2, Multiplier).
+
+    %write(Feedback1), write(Feedback2), write(' (Multiplier: '), write(Multiplier), write(')'), nl.
 
 % Example usage:
 % Call this query with your input parameters to get the feedback and multiplier
--- parse_IE.py
+++ parse_IE.py
@@ -8,9 +8,15 @@ def askInferenceEngine(fol):
     prolog1.consult('IE.pl')  # Replace 'your_prolog_file.pl' with the actual file path
 
     #TODO-INSIDE-PROLOG: Handle cases like P1: Squirtle P2: Charmander and user command is "use water against Bulbasaur" -> in reality its getting the foe pokemon always no matter what
+    #TODO-INSIDE-PROLOG: Now checking NPType with Etype for movements and NPType or VerbType for dodge/defend. Is this right? Check with Mikel
 
     # Query Prolog to get feedback1, feedback2, and multiplier
     results = list(prolog1.query(fol))
+    print("Results list:", results)
+    i=0
+    for result in results:
+        print(f"Result {i}: {result}")
+        i+=1
     result = results[0]
     feedback1 = result['Feedback1']
     feedback2 = result['Feedback2']
