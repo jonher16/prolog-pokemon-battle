8f2d723161731fcb0f491f2626c877d9fc005c9b
miklame24 <miklame24@gmail.com>
2023-11-09 17:00:03 +0900

    AType inserted

--- IE.pl
+++ IE.pl
@@ -10,9 +10,9 @@ verbinpast(P1, VerbWord, NPWords, Feedback1) :-
 
 % Define the feedback/5 predicate to construct Feedback2 and Multiplier
 
-feedback(VerbType, NPType, EType, Intensity, Direction, Feedback, Multiplier) :-
+feedback(VerbType, NPType, EType, Intensity, Direction, Feedback, Multiplier, P1) :-
 
-     % MOVEMENT
+    % MOVEMENT
     %(Direction == 'forward', Feedback = 'The Pokémon moved closer to the enemy!', Multiplier = 'none', !);
     %(Direction == 'backward', Feedback = 'The Pokémon moved away from the enemy!', Multiplier = 'none', !);
 
@@ -24,53 +24,112 @@ feedback(VerbType, NPType, EType, Intensity, Direction, Feedback, Multiplier) :-
     %(VerbType == 'defend', Feedback = 'The pokemon successfully defended itself from the enemy attack!', Multiplier = 'none', !);
     %(VerbType == 'nodefend', Feedback = 'The Pokémon failed at defending! The defense was broken!', Multiplier = 'none', !);
 
+    % Handle the "Unknown" case
+    % (VerbType == 'unknown', NPType == 'unknown', Feedback = 'The pokemon is looking at you with a confused face.');
+    (
+    % Decide AType according to priority
+
+    % Normal attack type
+    (VerbType == 'normal', NPType == 'unknown', AType == 'normal', Error = 0);
+    (VerbType == 'unknown', NPType == 'normal', AType == 'normal', Error = 0);
+    
+    % Fire attack type
+    (VerbType == 'fire', NPType == 'normal', AType == 'fire', Error = 0);
+    (VerbType == 'normal', NPType == 'fire', AType == 'fire', Error = 0);
+    (VerbType == 'fire', NPType == 'unknown', AType == 'fire', Error = 0);
+    (VerbType == 'unknown', NPType == 'fire', AType == 'fire', Error = 0);
+
+    % Grass attack type
+    (VerbType == 'grass', NPType == 'normal', AType == 'grass', Error = 0);
+    (VerbType == 'normal', NPType == 'grass', AType == 'grass', Error = 0);
+    (VerbType == 'grass', NPType == 'unknown', AType == 'grass', Error = 0);
+    (VerbType == 'unknown', NPType == 'grass', AType == 'grass', Error = 0);
+
+    % Water attack type
+    (VerbType == 'water', NPType == 'normal', AType == 'water', Error = 0);
+    (VerbType == 'normal', NPType == 'water', AType == 'water', Error = 0);
+    (VerbType == 'water', NPType == 'unknown', AType == 'water', Error = 0);
+    (VerbType == 'unknown', NPType == 'water', AType == 'water', Error = 0);
+
+    % Special attack errors
+    (VerbType == 'fire', NPType == 'water', Error = 1);
+    (VerbType == 'water', NPType == 'fire', Error = 1);
+    (VerbType == 'fire', NPType == 'grass', Error = 1);
+    (VerbType == 'grass', NPType == 'fire', Error = 1);
+    (VerbType == 'water', NPType == 'grass', Error = 1);
+    (VerbType == 'grass', NPType == 'water', Error = 1).
+
+    ),
+    (
+    (Error = 1);
+
+    % Check is AType matches pokemon type and return error
+    (P1 == 'Charmander', AType == 'water', Error = 1);
+    (P1 == 'Charmander', AType == 'grass', Error = 1);
+    (P1 == 'Charmander', AType == 'fire', Error = 0);
+
+    (P1 == 'Squirtle', AType == 'fire', Error = 1);
+    (P1 == 'Squirtle', AType == 'grass', Error = 1);
+    (P1 == 'Squirtle', AType == 'water', Error = 0);
+
+    (P1 == 'Bulbasaur', AType == 'water', Error = 1);
+    (P1 == 'Bulbasaur', AType == 'fire', Error = 1);
+    (P1 == 'Bulbasaur', AType == 'grass', Error = 0).
+    ),
+    (
+    % Error feedback for each pokemon
+    (Error = 1, P1 = 'Charmander', Feedback = 'Charmander did not understand the instruction.', Multiplier = 'none');
+    (Error = 1, P1 = 'Squirtle', Feedback = 'Squirtle did not understand the instruction.', Multiplier = 'none');
+    (Error = 1, P1 = 'Bulbasaur', Feedback = 'Bulbasaur did not understand the instruction.', Multiplier = 'none');
+
     % WATER -> FIRE
-    (NPType == 'water', EType == 'fire', Intensity == 'strong', Feedback = 'The attack was super effective!', Multiplier = 3);
-    (NPType == 'water', EType == 'fire', Intensity == 'none', Feedback = 'The attack was very effective.', Multiplier = 2);
-    (NPType == 'water', EType == 'fire', Intensity == 'weak', Feedback = 'The attack was effective but weak.', Multiplier = 1);
+    (AType == 'water', EType == 'fire', Intensity == 'strong', Feedback = 'The attack was super effective!', Multiplier = 3);
+    (AType == 'water', EType == 'fire', Intensity == 'none', Feedback = 'The attack was very effective.', Multiplier = 2);
+    (AType == 'water', EType == 'fire', Intensity == 'weak', Feedback = 'The attack was effective but weak.', Multiplier = 1);
 
     % FIRE -> WATER
-    (NPType == 'fire', EType == 'water', Intensity == 'strong', Feedback = 'The attack was not very effective but dealt some damage!', Multiplier = 1);
-    (NPType == 'fire', EType == 'water', Intensity == 'none', Feedback = 'The attack was not very effective...', Multiplier = 0.5);
-    (NPType == 'fire', EType == 'water', Intensity == 'weak', Feedback = 'The attack was not effective at all...', Multiplier = 0.25);
+    (AType == 'fire', EType == 'water', Intensity == 'strong', Feedback = 'The attack was not very effective but dealt some damage!', Multiplier = 1);
+    (AType == 'fire', EType == 'water', Intensity == 'none', Feedback = 'The attack was not very effective...', Multiplier = 0.5);
+    (AType == 'fire', EType == 'water', Intensity == 'weak', Feedback = 'The attack was not effective at all...', Multiplier = 0.25);
 
     % WATER -> GRASS
-    (NPType == 'water', EType == 'grass', Intensity == 'strong', Feedback = 'The attack was not very effective but dealt some damage!', Multiplier = 1);
-    (NPType == 'water', EType == 'grass', Intensity == 'none', Feedback = 'The attack was not very effective...', Multiplier = 0.5);
-    (NPType == 'water', EType == 'grass', Intensity == 'weak', Feedback = 'The attack was not effective at all...', Multiplier = 0.25);
+    (AType == 'water', EType == 'grass', Intensity == 'strong', Feedback = 'The attack was not very effective but dealt some damage!', Multiplier = 1);
+    (AType == 'water', EType == 'grass', Intensity == 'none', Feedback = 'The attack was not very effective...', Multiplier = 0.5);
+    (AType == 'water', EType == 'grass', Intensity == 'weak', Feedback = 'The attack was not effective at all...', Multiplier = 0.25);
 
     % GRASS -> WATER
-    (NPType == 'grass', EType == 'water', Intensity == 'strong', Feedback = 'The attack was super effective!', Multiplier = 3);
-    (NPType == 'grass', EType == 'water', Intensity == 'none', Feedback = 'The attack was very effective.', Multiplier = 2);
-    (NPType == 'grass', EType == 'water', Intensity == 'weak', Feedback = 'The attack was effective but weak.', Multiplier = 1);
+    (AType == 'grass', EType == 'water', Intensity == 'strong', Feedback = 'The attack was super effective!', Multiplier = 3);
+    (AType == 'grass', EType == 'water', Intensity == 'none', Feedback = 'The attack was very effective.', Multiplier = 2);
+    (AType == 'grass', EType == 'water', Intensity == 'weak', Feedback = 'The attack was effective but weak.', Multiplier = 1);
 
     % FIRE -> GRASS
-    (NPType == 'fire', EType == 'grass', Intensity == 'strong', Feedback = 'The attack was super effective!', Multiplier = 3);
-    (NPType == 'fire', EType == 'grass', Intensity == 'none', Feedback = 'The attack was very effective.', Multiplier = 2);
-    (NPType == 'fire', EType == 'grass', Intensity == 'weak', Feedback = 'The attack was effective but weak.', Multiplier = 1);
+    (AType == 'fire', EType == 'grass', Intensity == 'strong', Feedback = 'The attack was super effective!', Multiplier = 3);
+    (AType == 'fire', EType == 'grass', Intensity == 'none', Feedback = 'The attack was very effective.', Multiplier = 2);
+    (AType == 'fire', EType == 'grass', Intensity == 'weak', Feedback = 'The attack was effective but weak.', Multiplier = 1);
 
     % GRASS -> FIRE
-    (NPType == 'grass', EType == 'fire', Intensity == 'strong', Feedback = 'The attack was not very effective but dealt some damage!', Multiplier = 1);
-    (NPType == 'grass', EType == 'fire', Intensity == 'none', Feedback = 'The attack was not very effective...', Multiplier = 0.5);
-    (NPType == 'grass', EType == 'fire', Intensity == 'weak', Feedback = 'The attack was not effective at all...', Multiplier = 0.25);
+    (AType == 'grass', EType == 'fire', Intensity == 'strong', Feedback = 'The attack was not very effective but dealt some damage!', Multiplier = 1);
+    (AType == 'grass', EType == 'fire', Intensity == 'none', Feedback = 'The attack was not very effective...', Multiplier = 0.5);
+    (AType == 'grass', EType == 'fire', Intensity == 'weak', Feedback = 'The attack was not effective at all...', Multiplier = 0.25);
 
     % WATER -> WATER
-    (NPType == 'water', EType == 'water', Intensity == 'strong', Feedback = 'The attack was not very effective but dealt some damage!', Multiplier = 1);
-    (NPType == 'water', EType == 'water', Intensity == 'none', Feedback = 'The attack was not very effective...', Multiplier = 0.5);
-    (NPType == 'water', EType == 'water', Intensity == 'weak', Feedback = 'The attack was not effective at all...', Multiplier = 0.25);
+    (AType == 'water', EType == 'water', Intensity == 'strong', Feedback = 'The attack was not very effective but dealt some damage!', Multiplier = 1);
+    (AType == 'water', EType == 'water', Intensity == 'none', Feedback = 'The attack was not very effective...', Multiplier = 0.5);
+    (AType == 'water', EType == 'water', Intensity == 'weak', Feedback = 'The attack was not effective at all...', Multiplier = 0.25);
 
     % FIRE -> FIRE
-    (NPType == 'fire', EType == 'fire', Intensity == 'strong', Feedback = 'The attack was not very effective but dealt some damage!', Multiplier = 1);
-    (NPType == 'fire', EType == 'fire', Intensity == 'none', Feedback = 'The attack was not very effective...', Multiplier = 0.5);
-    (NPType == 'fire', EType == 'fire', Intensity == 'weak', Feedback = 'The attack was not effective at all...', Multiplier = 0.25);
+    (AType == 'fire', EType == 'fire', Intensity == 'strong', Feedback = 'The attack was not very effective but dealt some damage!', Multiplier = 1);
+    (AType == 'fire', EType == 'fire', Intensity == 'none', Feedback = 'The attack was not very effective...', Multiplier = 0.5);
+    (AType == 'fire', EType == 'fire', Intensity == 'weak', Feedback = 'The attack was not effective at all...', Multiplier = 0.25);
 
     % GRASS -> GRASS
-    (NPType == 'grass', EType == 'grass', Intensity == 'strong', Feedback = 'The attack was not very effective but dealt some damage!', Multiplier = 1);
-    (NPType == 'grass', EType == 'grass', Intensity == 'none', Feedback = 'The attack was not very effective...', Multiplier = 0.5);
-    (NPType == 'grass', EType == 'grass', Intensity == 'weak', Feedback = 'The attack was not effective at all...', Multiplier = 0.25);
+    (AType == 'grass', EType == 'grass', Intensity == 'strong', Feedback = 'The attack was not very effective but dealt some damage!', Multiplier = 1);
+    (AType == 'grass', EType == 'grass', Intensity == 'none', Feedback = 'The attack was not very effective...', Multiplier = 0.5);
+    (AType == 'grass', EType == 'grass', Intensity == 'weak', Feedback = 'The attack was not effective at all...', Multiplier = 0.25);
 
     % ALL-CASE
     (Feedback = 'Unknown action', Multiplier = 'none').
+    )
 
 % Query to get the feedback and multiplier based on input parameters
 parse_input(VerbType, NPType, Intensity, Direction, VerbWord, NPWords, EType, P1, P2, Feedback1, Feedback2, Multiplier) :-
@@ -81,7 +140,7 @@ parse_input(VerbType, NPType, Intensity, Direction, VerbWord, NPWords, EType, P1
     write('Intensity: '), write(Intensity), nl,
     write('Direction: '), write(Direction), nl,
     verbinpast(P1, VerbWord, NPWords, Feedback1),
-    feedback(VerbType, NPType, EType, Intensity, Direction, Feedback2, Multiplier).
+    feedback(VerbType, NPType, EType, Intensity, Direction, Feedback2, Multiplier, P1).
 
     %write(Feedback1), write(Feedback2), write(' (Multiplier: '), write(Multiplier), write(')'), nl.
 
